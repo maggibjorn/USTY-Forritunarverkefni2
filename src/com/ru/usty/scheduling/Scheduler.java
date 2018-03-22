82f5fe3695d3ca3485033762f4bc3b87c0dccde3
maggibjorn <mb14@ru.is>
2018-03-22 14:30:28 +0000

    For some reason RR stops when comments are left out in timer class, good skeleton to start implementing SRT

--- src/com/ru/usty/scheduling/Scheduler.java
+++ src/com/ru/usty/scheduling/Scheduler.java
@@ -187,31 +187,16 @@ public class Scheduler {
 				e.printStackTrace();
 			}
 		}
-		if (processID != currentRunningProcessID) {
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			System.out.println("Correct: " + processID + "----" + "currentRunningProcessID " + currentRunningProcessID);
-			
-		}
+		
 		if (!this.readyQueue.isEmpty()) {
-			System.out.println("*****Entering process finished*****");
-			System.out.println(this.readyQueue);
 			// The queue is not empty and there is a process waiting for the processor
 			currentRunningProcessID = this.readyQueue.remove();
 			this.processExecution.switchToProcess(currentRunningProcessID);
 			this.systemTime = System.currentTimeMillis();
-			
-			System.out.println("Process leaves: " + processID);
-			System.out.println("Next process: " + currentRunningProcessID);
-			System.out.println(this.readyQueue);
 		} else {
 			// No process on queue
 			someoneRunning = false;
 		}
-		System.out.println("*****Leaving process finished*****");
 		if (this.switchMutex != null) {
 			this.switchMutex.release();
 		}
